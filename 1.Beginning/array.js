501697e985cfa02a72366f770ae765ff33a49e0b
KKian-21 <kian.fetic20@gmail.com>
2021-02-14 10:59:45 +0100

    delete other folders

--- 1.Beginning/array.js
+++
@@ -1,9 +0,0 @@
-// const listUsers = ['Johny','John Snow','You']
-
-// listUsers.push('NEW');
-// listUsers.unshift('Paul')
-
-
-// console.log(listUsers);
-// console.log(listUsers.length);
-// console.log(listUsers.indexOf('Johny'));
--- 1.Beginning/variables.js
+++
@@ -1,6 +0,0 @@
-// let inbox1 = 100;
-// let inbox2 = 20;
-
-// let allEmails = inbox1 + inbox2;
-
-// alert(allEmails);
